16ae1f24a99937cc76a6806db8aada79b8119014
KZ-DE <nebulacom005@gmail.com>
2023-04-24 18:30:43 +0700

    a

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { Parallax } from "./parallax/parallax-2/Parallax";
+// import { Parallax } from "./parallax/parallax-1/Parallax";
+
+function App() {
+  return <Parallax />;
+}
+
+export default App;
